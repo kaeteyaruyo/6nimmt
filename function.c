05045903ac321edafe313581b09c3933d0aaa09a
Solo-steven <ssteven19@kimo.com>
2021-01-15 00:34:21 +0800

    Change whole program , file structure remain the same

--- function.c
+++ function.c
@@ -1,129 +1,291 @@
-#include"header.h"
+#include "header.h"
 
-extern card CARDS[104],*player[10],*table[4];
-extern int score[10];
+/* -------- Card -------- */
+void Init_Card(Card* card){
+    card->head=NULL;
+    card->tail=NULL;
+}
 
-void print_link(card *link){ //link for link list you want to print
+void Init_Card_Node(Card_Node* node){
+  node->index=-1; 
+  node->num=0;
+  node->next = NULL;
+}
 
- card *buf=link;
- while(buf!=NULL){
-  printf("|%d,%d| ", buf->num, buf->head);
-  buf=buf->next;
- };
- printf("\n");
+void Setup_Card_Node(Card_Node* node, int number){
+    node->num=number;
+}
+
+void AddCard(Card* card, int number){
+    Card_Node* tmp=(Card_Node*)malloc(sizeof(Card_Node));
+    Init_Card_Node(tmp); 
+    Setup_Card_Node(tmp, number);
+
+    if(card->head==NULL){
+       card->head=tmp;
+       card->tail=tmp;
+       return;
+    }
+    Card_Node* cur=card->head, *pre=NULL;
+    while(cur!=NULL && cur->num < tmp->num){
+        pre=cur;
+        cur=cur->next;
+    }
+    if(cur==NULL){
+        card->tail->next = tmp;
+        card->tail=tmp;
+        return;
+    }
+    if(pre==NULL){
+      tmp->next=card->head;
+      card->head=tmp;
+       return;
+    }
+    tmp->next=cur;
+    pre->next=tmp;
 
 }
 
-card* choose_card(card* link, int n){ //link for a row contain the card chosen, n for the card's position
-
- int i;
-
- for(i=1; i<n; i++) link=link->next;
- return link; 
-
+int PopCard(Card* card){
+    if(card->head==NULL)
+      return -1;
+    if(card->head==card->tail)
+      card->tail=NULL;   
+    Card_Node* tmp=card->head;
+    int num=tmp->num;
+    card->head=card->head->next;
+    free(tmp);
+    return num;
 }
 
-int select_row(card* table[], int C, int P){ //table, C for the card chosen, P for the player
-
- int i,buf=0,entry=-1,last[4]={0};
-
- for(i=0; i<4; i++){
-  card* buf=table[i];
-  while(buf->next!=NULL) buf=buf->next;
-  last[i]=buf->num;
- };
-
- for(i=0; i<4; i++)
-  if(last[i]<C && last[i]>buf){
-   buf=last[i];
-   entry=i;
-  };
-
- if(entry>=0) return entry;
- else{
-  if(P==0){
-   printf("No row can your card insert, please choose a row to eat.(key a number from 1-4.)\n");
-   scanf("%d",&entry);}
-  else entry=1+rand()%4;
-  score[P]+=count_head(&table[entry-1]);
-  return entry-1;
- }; 
-
-}
-
-card* insert(card* h, card* new){ //h for the row you want to insert, new for the card inserted
-
- card* buf=h;
-
- if(h==NULL){
-  return new;
- };
-
- if(new->num <= h->num){
-  new->next=h;
-  h->prev=new;
-  return new;
- };
-
- while(buf->next!=NULL && buf->next->num < new->num) buf=buf->next;
-
- if(buf->next!=NULL){
-  buf->next->prev=new;
-  new->next=buf->next;
- };
- new->prev=buf;
- buf->next=new;
- return h;
-
-}
-
-card* delete(card** link, int C){
-
- card* buf=*link;
-
- while(buf!=NULL && buf->num!=C) buf=buf->next;
-
- if(buf==NULL) return NULL;
- if(buf->prev==NULL) {
-  if(buf->next!=NULL)buf->next->prev=NULL;
-  *link=buf->next;
-  buf->next=NULL;
-  return buf;
- };
- if(buf->next!=NULL) buf->next->prev=buf->prev;
- buf->prev->next=buf->next;
- buf->prev=NULL;
- buf->next=NULL;
- return buf;
-
+void Prinf_Card(Card* card){
+    Card_Node* tmp=card->head;
+    while(tmp!=NULL){
+        printf("%d ", tmp->num);
+        tmp=tmp->next;
+    }
 }
 
 
-int count_table(card* link){
+/* for wait cards */
+void AddCard_With_index(Card* card, int number, int index){
+    Card_Node* tmp=(Card_Node*)malloc(sizeof(Card_Node));
+    Init_Card_Node(tmp); 
+    Setup_Card_Node(tmp, number);
+    tmp->index=index;
 
- int count=0;
-
- while(link!=NULL){
-  count++;
-  link=link->next;
- };
-
- return count;
+    if(card->head==NULL){
+       card->head=tmp;
+       card->tail=tmp;
+       return;
+    }
+    Card_Node* cur=card->head, *pre=NULL;
+    while(cur!=NULL && cur->num < tmp->num){
+        pre=cur;
+        cur=cur->next;
+    }
+    if(cur==NULL){
+        card->tail->next = tmp;
+        card->tail=tmp;
+        return;
+    }
+    if(pre==NULL){
+      tmp->next=card->head;
+      card->head=tmp;
+       return;
+    }
+    tmp->next=cur;
+    pre->next=tmp;
 
 }
 
-int count_head(card** link){
+int Get_Head_Number(Card* card){
+    if(card->head==NULL)
+      return -1;  
+    return card->head->num;
+}
 
- int count=0;
- card* buf=*link;
+int Get_Head_index(Card* card){
+    if(card->head==NULL)
+      return -1;  
+    return card->head->index;
+}
 
- do{
-  count+=buf->head;
-  buf=buf->next;
- }while(buf!=NULL);
 
- *link=NULL;
+/* --- Computer --- */
 
- return count;
+void Init_Computer(Computer* computer){
+      if(computer==NULL)
+        return;
+      //printf("Init Computer\n");
+      computer->card=(Card*)malloc(sizeof(Card));
+      Init_Card(computer->card);
+      computer->score=0;
+}
 
+void Add_Computer_Card(Computer* computer,int number){
+    AddCard(computer->card, number);
+}
+
+int  Select_Computer_Card(Computer* computer){
+    return PopCard(computer->card);
+}
+
+void Add_Computer_Score(Computer* compuer, int number){
+     compuer->score+=number;
+}
+
+void Printf_Computer_Card(Computer* computer){
+    Prinf_Card(computer->card); 
+    printf("score : %d.",computer->score);
+}
+
+
+/* ---------- Player ---------- */
+void DeleteCard(Card* card, int number){
+    Card_Node* cur=card->head, *pre=NULL;
+     while(cur!=NULL && cur->num!=number){
+        pre=cur;
+        cur=cur->next;
+    }
+    if(cur==NULL)
+     return;
+    if(cur==card->head)
+     {
+         PopCard(card);
+         return;
+     }   
+    pre->next=cur->next;
+    free(cur);  
+}
+
+Card_Node* SearchCard(Card* card, int number){
+    Card_Node* tmp=card->head;
+    while(tmp!=NULL){
+        if(tmp->num==number)
+          return tmp;
+        tmp=tmp->next; 
+    }
+  return NULL;
+}
+
+int SelectCard(Card* card, int number){
+  Card_Node* tmp=SearchCard(card, number);
+  if(tmp==NULL)
+     return -1;
+   DeleteCard(card, number);
+  return number;
+}
+
+void  Init_Player(Player* player){
+     player->card=(Card*)malloc(sizeof(Card));
+     Init_Card(player->card);
+     player->score=0;
+}
+
+void Add_Player_Card(Player* player, int num){
+    AddCard(player->card,num);
+}
+
+void Print_Player_Card(Player* player){
+    printf("Your Turn !!\nThere is your cards : ");
+    Prinf_Card(player->card);
+    printf("\n"); 
+    printf("There is your score : %d\n",player->score);
+}
+
+int Select_Player_Card(Player* player){
+    int tmp=0;
+    printf("Your choise : ");
+    scanf("%d",&tmp);
+    int n=SelectCard(player->card, tmp);
+    if(n==-1)
+    {
+        printf("Worng Choise , Pleae Choise again . \n");
+        return Select_Player_Card(player);
+    }
+    return n;
+}
+
+void Add_Player_Score(Player* player, int add_score){
+    player->score+=add_score;
+}
+
+
+/* ---------- Table ---------- */
+
+int Card_Filled(Card* card){
+      Card_Node*  cur=card->head;
+      int n=0;
+      while (cur!=NULL){
+             n++;
+             cur=cur->next;
+      }
+      if(n>=6)
+        return 1;
+      return 0;  
+}
+
+int Sum_Card(Card* card){
+     int sum=0;
+     for(int i=0;i<5;++i)
+        sum+=PopCard(card);
+     return sum;
+}
+
+void Init_Table(Table* table){
+     //printf("init Table\n");
+     for(int i=0;i<4;++i){
+       table->card[i]=(Card*) malloc(sizeof(Card));  
+       Init_Card(table->card[i]);
+     }
+}
+
+void Set_Table(Table* table,int * num){
+    for(int i=0;i<NUM;++i)
+       AddCard(table->card[i], num[i]); 
+}
+
+int Add_Card_To_Table(Table* table,int num){
+    int pointer=-1;
+    int diffence=-1;
+    for(int i=0;i<NUM;++i){
+        if(table->card[i]->tail->num < num){
+            if(diffence==-1){
+                   diffence=num- table->card[i]->tail->num;
+                   pointer=i;
+            }
+            if(diffence > num- table->card[i]->tail->num){
+                pointer=i;
+                diffence=num- table->card[i]->tail->num;
+            }
+        }
+    }
+
+    if(pointer==-1){
+        return -1;
+    }
+    AddCard(table->card[pointer], num);
+    if(Card_Filled(table->card[pointer])){
+         return Sum_Card(table->card[pointer]);
+    }
+    return 0;
+}
+
+int Reset_Card_On_Table(Table* table, int index, int num){
+    int tmp=PopCard(table->card[index]) , sum=0;
+    while(tmp!=-1){
+        sum+=tmp;
+        tmp=PopCard(table->card[index]);
+    }
+    AddCard(table->card[index],num) ;
+    return sum;   
+} 
+
+void Print_Table(Table* table){
+    for(int i=0;i<NUM;++i){
+        printf("*-- ");
+        Prinf_Card(table->card[i]);
+        printf("\n");
+    }
 }
